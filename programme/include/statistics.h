cd293b7efed5b7034ee4426527dea769157a12f2
qberal <quentin.beral@insa-rouen.fr>
2023-12-02 16:08:09 +0100

    add statistics.h

--- programme/include/statistics.h
+++ programme/include/statistics.h
@@ -1,6 +1,25 @@
 #ifndef STATISTICS_H
 #define STATISTICS_H
 
+#define MAX 256
 
+typedef struct{
+    Byte element[MAX];
+    int nbOfElements;
+}S_Statictics;
+
+S_Statictics S_statistics();
+
+bool S_isEmpty(S_Statictics stats);
+
+bool S_contains(S_Statictics stats, Byte element);
+
+int S_getCount(S_Statictics stats, Byte element);
+
+void S_incCount(S_Statictics* stats, Byte element);
+
+int S_length(S_Statictics* stats);
+
+Byte S_getElement(S_Statictics stats, int pos);
 
 #endif // STATISTICS_H
