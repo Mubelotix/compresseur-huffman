a28958fb684b17c5827459f4681388fb1d2b49ed
Mubelotix <mubelotix@gmail.com>
2023-12-05 10:06:36 +0100

    Create priorityQueue.h

---
+++ programme/include/priorityQueue.h
@@ -0,0 +1,19 @@
+#ifndef STATISTICS_H
+#define STATISTICS_H
+
+#include "huffmanTree.h"
+
+#define MAX 256
+
+struct PriorityQueue {
+    HuffmanTree elements[MAX];
+    unsigned int length;
+};
+
+PriorityQueue newPriorityQueue();
+int PQ_getInsertionIndex(PriorityQueue queue, HuffmanTree ht);
+void PQ_shiftRight(PriorityQueue *queue, int index);
+void PQ_insertTree(PriorityQueue *queue, HuffmanTree ht);
+HuffmanTree PQ_popTree(PriorityQueue *queue);
+
+#endif
