51757fef7cb7d3c82d85527a5abd1fda9e08a688
Florent TARIOLLE <florent.tariolle@insa-rouen.fr>
2023-12-05 10:55:24 +0100

    Header for Decompress #41

--- programme/include/byte.h
+++ programme/include/byte.h
@@ -1,18 +1,18 @@
 #ifndef BYTE_H
 #define BYTE_H
 
-typedef enum{0,1} Bit;
+typedef enum{0,1} Bit_Bit;
 
 typedef struct{
-    Bit array[8];
-}Byte;
+    Bit_Bit array[8];
+}B_Byte;
 
-Byte B_byte(Bit b1, Bit b2, Bit b3, Bit b4, Bit b5, Bit b6, Bit b7, Bit b8);
+B_Byte B_byte(Bit_Bit b1, Bit_Bit b2, Bit_Bit b3, Bit_Bit b4, Bit_Bit b5, Bit_Bit b6, Bit_Bit b7, Bit_Bit b8);
 
-Bit B_getBit(Byte byte, unsigned int i);
+Bit_Bit B_getBit(B_Byte byte, unsigned int i);
 
-void B_setBit(Byte byte, unsigned int i, Bit bit);
+void B_setBit(B_Byte* byte, unsigned int i, Bit_Bit bit);
 
-unsigned int B_byteToNatural(Byte byte);
+unsigned int B_byteToNatural(B_Byte byte);
 
 #endif
---
+++ programme/include/decompress.h
@@ -0,0 +1,6 @@
+#ifndef DECOMPRESS_H
+#define DECOMPRESS_H
+
+void D_decompress(FILE* f);
+
+#endif
