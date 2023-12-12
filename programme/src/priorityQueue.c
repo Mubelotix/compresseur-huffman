cada4a4df112008208120fb6ddebbd3f4d51098a
Mubelotix <mubelotix@gmail.com>
2023-12-12 10:40:42 +0100

    Add error handling to priorityQueue.c

--- programme/src/priorityQueue.c
+++ programme/src/priorityQueue.c
@@ -1,4 +1,6 @@
 #include "priorityQueue.h"
+#include "huffmanTree.h"
+#include <errno.h>
 
 PQ_PriorityQueue PQ_new() {
     PQ_PriorityQueue result;
@@ -7,6 +9,7 @@ PQ_PriorityQueue PQ_new() {
 }
 
 int PQ_getInsertionIndex(PQ_PriorityQueue *queue, HT_HuffmanTree ht) {
+    // TODO: having binary search here would be nice
     int i = 0;
     while (i < queue->length && HT_getOccurence(ht) > HT_getOccurence(queue->elements[i])) {
         i++;
@@ -15,11 +18,11 @@ int PQ_getInsertionIndex(PQ_PriorityQueue *queue, HT_HuffmanTree ht) {
 }
 
 void PQ_shiftRight(PQ_PriorityQueue *queue, int index) {
-    // Overflow handling with erno
     if (queue->length == MAX) {
+        errno = EOVERFLOW;
         return;
     }
-    queue->length++; // TODO handle overflow
+    queue->length++;
     int i = queue->length;
     while (i > index) {
         queue->elements[i] = queue->elements[i-1];
@@ -29,11 +32,18 @@ void PQ_shiftRight(PQ_PriorityQueue *queue, int index) {
 
 void PQ_insertTree(PQ_PriorityQueue *queue, HT_HuffmanTree ht) {
     int i = PQ_getInsertionIndex(queue, ht);
-    PQ_shiftRight(queue, i); // Handle error
+    PQ_shiftRight(queue, i);
+    if (errno == EOVERFLOW) {
+        return;
+    }
     queue->elements[i] = ht;
 }
 
 HT_HuffmanTree PQ_popTree(PQ_PriorityQueue *queue) {
+    if (queue->length == 0) {
+        errno = EOVERFLOW;
+        return HT_new();
+    }
     queue->length--;
     return queue->elements[queue->length];
 }
