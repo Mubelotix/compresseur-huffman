244581d8873e33aba789f4910e9f560bafe7834e
Florent TARIOLLE <florent.tariolle@insa-rouen.fr>
2023-12-19 10:31:41 +0100

    fixed missing comas

--- programme/src/tests/testHuffmanTree.c
+++ programme/src/tests/testHuffmanTree.c
@@ -3,48 +3,48 @@
 #include "byte.h"
 
 void test_getRightChild() {
-    ht = HT_createTree(HT_createNode(HT_createLeaf(0), HT_createLeaf(0)))
+    ht = HT_createNode(HT_createLeaf(0), HT_createLeaf(0));
     rc = HT_getRightChild(ht);
     CU_ASSERT_EQUAL(ht.rightChild, b);
 
-    ht = HT_createTree(HT_createNode(HT_createLeaf(0), HT_createLeaf(42)))
+    ht = HT_createNode(HT_createLeaf(0), HT_createLeaf(42));
     rc = HT_getRightChild(ht);
     CU_ASSERT_EQUAL(ht.rightChild, b);
 }
 
 void test_getLeftChild() {
-    ht = HT_createTree(HT_createNode(HT_createLeaf(0), HT_createLeaf(0)))
+    ht = HT_createNode(HT_createLeaf(0), HT_createLeaf(0));
     rc = HT_getLeftChild(ht);
     CU_ASSERT_EQUAL(ht.leftChild, b);
 
-    ht = HT_createTree(HT_createNode(HT_createLeaf(0), HT_createLeaf(42)))
+    ht = HT_createNode(HT_createLeaf(0), HT_createLeaf(42));
     rc = HT_getLeftChild(ht);
     CU_ASSERT_EQUAL(ht.leftChild, b);
 }
 
 void test_getByte() {
-    byte = B_byte( 0, 1, 1, 1, 0, 0, 0, 1)
-    leaf = HT_createLeaf(byte)
+    byte = B_byte( 0, 1, 1, 1, 0, 0, 0, 1);
+    leaf = HT_createLeaf(byte);
     CU_ASSERT_EQUAL(getByte(leaf), byte);
 
-    byte = B_byte(1, 1, 1, 0, 0, 1, 1, 1)
-    leaf = HT_createLeaf(byte)
+    byte = B_byte(1, 1, 1, 0, 0, 1, 1, 1);
+    leaf = HT_createLeaf(byte);
     CU_ASSERT_EQUAL(getByte(leaf), byte);
 
-    byte = B_byte(0, 0, 0, 0, 0, 0, 0, 0)
-    leaf = HT_createLeaf(byte)
+    byte = B_byte(0, 0, 0, 0, 0, 0, 0, 0);
+    leaf = HT_createLeaf(byte);
     CU_ASSERT_EQUAL(getByte(leaf), byte);
 
-    byte = B_byte(1, 1, 1, 1, 1, 1, 1, 1)
-    leaf = HT_createLeaf(byte)
+    byte = B_byte(1, 1, 1, 1, 1, 1, 1, 1);
+    leaf = HT_createLeaf(byte);
     CU_ASSERT_EQUAL(getByte(leaf), byte);
 }
 
 void test_createLeaf() {
-    leaf = HT_createLeaf(0)
+    leaf = HT_createLeaf(0);
     CU_ASSERT_EQUAL(HT_getOccurence(leaf), 0);
 
-    leaf = HT_createLeaf(42)
+    leaf = HT_createLeaf(42);
     CU_ASSERT_EQUAL(HT_getOccurence(leaf), 42);
 }
 
@@ -57,10 +57,10 @@ void test_createTree() {
 }
 
 void test_isALeaf() {
-    ht = HT_createNode(HT_createLeaf(0), HT_createLeaf(0))
+    ht = HT_createNode(HT_createLeaf(0), HT_createLeaf(0));
     CU_ASSERT_EQUAL(HT_isALeaf(ht), 0);
 
-    ht = HT_createLeaf(0)
+    ht = HT_createLeaf(0);
     CU_ASSERT_EQUAL(HT_isALeaf(ht), 1);
 }
 
@@ -69,7 +69,11 @@ void test_getOccurence() {
 }
 
 void test_createNode() {
-    // TO DO
+    leaf1 = HT_createLeaf(0);
+    leaf2 = HT_createLeaf(42);
+    node = HT_createNode(leaf1, leaf2);
+    CU_ASSERT_EQUAL(leaf1, HT_getLeftChild(node));
+    CU_ASSERT_EQUAL(leaf2, HT_getLeftChild(node));
 }
 
 int main() {
