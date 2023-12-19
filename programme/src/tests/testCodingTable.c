176dd3b098e94785e797084897dbc6663d336a00
houssam <houssam.limy@insa-rouen.fr>
2023-12-19 11:13:29 +0100

    Coding table test update

--- programme/src/tests/testCodingTable.c
+++ programme/src/tests/testCodingTable.c
@@ -1,5 +1,6 @@
 #include <CUnit/CUnit.h>
 #include "codingTable.h"
+#include "binaryCode.h"
 #include "byte.h"
 
 
@@ -14,27 +15,63 @@ void CT_testLength() {
     CT_CodingTable table;
     table.length = 5;
 
-    CU_ASSERT_EQUAL(CT_lenght(table),5);
+    CU_ASSERT_EQUAL(CT_length(table),5);
 }
 
 void CT_testContainsAndAdd() {
-    CT_CodingTable table = CT_codingTable();
+    CT_CodingTable table = CT_new();
+    BC_Bit b0 = BC_ZERO;
+    BC_Bit b1 = BC_ONE;
+    BC_BinaryCode code = BC_binaryCode();
+    BC_addBit(&code, b0);
+    BC_addBit(&code, b1);
+    BC_addBit(&code, b0);
+    BC_addBit(&code, b1);
+    B_Byte byte = B_byte(b0,b0,b1,b0,b1,b1,b1,b0);
+
+    CU_ASSERT_TRUE(! CT_contains(table,byte));
+
+    CT_add(&table,byte,code);
+
+    CU_ASSERT_TRUE(CT_contains(table,byte));
+    CU_ASSERT_EQUAL(CT_length(table),1);
 
 }
 
+void CT_testGetBinaryCode() {
+    CT_CodingTable table = CT_new();
+    BC_Bit b0 = BC_ZERO;
+    BC_Bit b1 = BC_ONE;
+    BC_BinaryCode code = BC_binaryCode();
+    BC_addBit(&code, b0);
+    BC_addBit(&code, b1);
+    BC_addBit(&code, b0);
+    BC_addBit(&code, b1);
+    B_Byte byte = B_byte(b0,b0,b1,b0,b1,b1,b1,b0);
+
+    CT_add(&table,byte,code);
+
+    CU_ASSERT_EQUAL(CT_getBinaryCode(table,byte),code);
+    //ne peut pas comparer deux BC faut comparer bit par bit
+}
+
+void CT_testGetByte() {
+    CT_CodingTable table = CT_new();
+    BC_Bit b0 = BC_ZERO;
+    BC_Bit b1 = BC_ONE;
+    BC_BinaryCode code = BC_binaryCode();
+    BC_addBit(&code, b0);
+    BC_addBit(&code, b1);
+    BC_addBit(&code, b0);
+    BC_addBit(&code, b1);
+    B_Byte byte = B_byte(b0,b0,b1,b0,b1,b1,b1,b0);
+
+    CT_add(&table,byte,code);
+
+    CU_ASSERT_EQUAL(CT_getByte(table,code),byte);
+}
 /*
-void testAddAndContains() {
-    CodingTable table = codingTable();
-    Byte byteToAdd = 65;  // Exemple de byte à ajouter
-    BinaryCode codeToAdd = "0101";  // Exemple de code binaire à ajouter
 
-    assert(!contains(table, byteToAdd));
-
-    add(&table, byteToAdd, codeToAdd);
-
-    assert(contains(table, byteToAdd));
-    assert(length(table) == 1);
-}
 
 void testGetBinaryCode() {
     CodingTable table = codingTable();
--- programme/src/tests/testPriorityQueue.c
+++ programme/src/tests/testPriorityQueue.c
@@ -3,36 +3,37 @@
 #include "huffmanTree.h"
 
 //test axiome : lenght(newPriorityQueue()) = 0
-void PQ_testNewPriorityQueue(void) {
+void PQ_testNewPriorityQueue() {
     PQ_PriorityQueue queue = PQ_new();
 
-    CU_ASSERT_TRUE(lenght(queue) = 0);
+    CU_ASSERT_EQUAL(PQ_lenght(queue) , 0);
 }
 
 //test axiome : isEmpty(newPriorityQueue())
-void PQ_testIsEmpty(void) {
+void PQ_testIsEmpty() {
     PQ_PriorityQueue queue = PQ_new();
 
-    CU_ASSERT_TRUE(isEmpty(newPriorityQueue()));
+    CU_ASSERT_TRUE(PQ_isEmpty(newPriorityQueue()));
 }
 
 //test axiome : lenght(insertTree(q, ht)) = lenght(q) + 1
-void PQ_insertTree(void) {
+void PQ_testInsertTree() {
     PQ_PriorityQueue queue = PQ_new();
     int occurence = 42;
     HT_HuffmanTree ht = HT_createTree(HT_createLeaf(occurence));
     PQ_insertTree(&queue, ht);
 
-    CU_ASSERT_TRUE(lenght(queue) = 1);
+    CU_ASSERT_EQUAL(PQ_lenght(queue) , 1);
 }
 
 //test axiome : lenght(popTree(q)) = lenght(q) − 1  &  popTree(insertTree(q, ht)) = ht
-void PQ_testPopTree(void) {
+void PQ_testPopTree() {
     PQ_PriorityQueue queue = PQ_new();
     int occurence = 42;
     HT_HuffmanTree ht = HT_createTree(HT_createLeaf(occurence));
     PQ_insertTree(&queue, ht);
     HT_HuffmanTree pop_ht = PQ_popTree(&queue);
 
-    CU_ASSERT_TRUE((lenght(queue) = 0) && (ht = pop_ht));
+    CU_ASSERT_EQUAL(PQ_lenght(queue) , 0);
+    CU_ASSERT_EQUAL(ht , pop_ht);
 }
