0d8ea95033d9721d3ed6893f8c5b2f0c01b75bec
Florent TARIOLLE <florent.tariolle@insa-rouen.fr>
2023-12-19 10:19:39 +0100

    minor changes to testHuffmanTree.c

--- programme/src/tests/testHuffmanTree.c
+++ programme/src/tests/testHuffmanTree.c
@@ -24,28 +24,28 @@ void test_getLeftChild() {
 
 void test_getByte() {
     byte = B_byte( 0, 1, 1, 1, 0, 0, 0, 1)
-    leaf = createLeaf(byte)
+    leaf = HT_createLeaf(byte)
     CU_ASSERT_EQUAL(getByte(leaf), byte);
 
     byte = B_byte(1, 1, 1, 0, 0, 1, 1, 1)
-    leaf = createLeaf(byte)
+    leaf = HT_createLeaf(byte)
     CU_ASSERT_EQUAL(getByte(leaf), byte);
 
     byte = B_byte(0, 0, 0, 0, 0, 0, 0, 0)
-    leaf = createLeaf(byte)
+    leaf = HT_createLeaf(byte)
     CU_ASSERT_EQUAL(getByte(leaf), byte);
 
     byte = B_byte(1, 1, 1, 1, 1, 1, 1, 1)
-    leaf = createLeaf(byte)
+    leaf = HT_createLeaf(byte)
     CU_ASSERT_EQUAL(getByte(leaf), byte);
 }
 
 void test_createLeaf() {
-    
-}
+    leaf = HT_createLeaf(0)
+    CU_ASSERT_EQUAL(HT_getOccurence(leaf), 0);
 
-void test_newNode() {
-    // TO DO
+    leaf = HT_createLeaf(42)
+    CU_ASSERT_EQUAL(HT_getOccurence(leaf), 42);
 }
 
 void test_destroy() {
@@ -57,10 +57,18 @@ void test_createTree() {
 }
 
 void test_isALeaf() {
-    // TO DO
+    ht = HT_createNode(HT_createLeaf(0), HT_createLeaf(0))
+    CU_ASSERT_EQUAL(HT_isALeaf(ht), 0);
+
+    ht = HT_createLeaf(0)
+    CU_ASSERT_EQUAL(HT_isALeaf(ht), 1);
 }
 
 void test_getOccurence() {
+    // hay un problemo de concepcion si si senor
+}
+
+void test_createNode() {
     // TO DO
 }
 
@@ -81,7 +89,7 @@ int main() {
         (CU_add_test(suite, "test_getLeftChild", test_getLeftChild) == NULL) ||
         (CU_add_test(suite, "test_getByte", test_getByte) == NULL) ||
         (CU_add_test(suite, "test_createLeaf", test_createLeaf) == NULL)) ||
-        (CU_add_test(suite, "test_newNode", test_newNode) == NULL)||
+        (CU_add_test(suite, "test_createNode", test_createNode) == NULL)||
         (CU_add_test(suite, "test_destroy", test_destroy) == NULL)||
         (CU_add_test(suite, "test_getOccurence", test_getOccurence) == NULL)||
         (CU_add_test(suite, "test_isALeaf", test_isALeaf) == NULL){
