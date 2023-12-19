516d0d823349bec4963d7a0ac58526317be747a0
qberal <quentin.beral@insa-rouen.fr>
2023-12-19 10:33:49 +0100

    update statistics.h

--- programme/include/statistics.h
+++ programme/include/statistics.h
@@ -1,41 +1,41 @@
-#ifndef STATISTICS_H
+∏∏#ifndef STATISTICS_H
 #define STATISTICS_H
 
 #include "byte.h"
 
-#define MAX 256
+#define S_MAX 256
 #define bool int
 
-/**
- * @brief S_Statistics
- * @param element : B_Byte
- * @param nbOfElements : int
-*/
-typedef struct{
-    B_Byte element[MAX];
+    /**
+     * @brief S_Statistics
+     * @param element : B_Byte
+     * @param nbOfElements : int
+     */
+    typedef struct
+{
+    int element[S_MAX];
     int nbOfElements;
-}S_Statistics;
-
+} S_Statistics;
 
 /**
  * @brief S_statistics constructor
- * @return S_Statistics 
-*/
+ * @return S_Statistics
+ */
 S_Statistics S_statistics();
 
 /**
  * @brief S_isEmpty
  * @param stats
  * @return true if stats is empty
-*/
+ */
 bool S_isEmpty(S_Statistics stats);
 
 /**
  * @brief S_contains
  * @param stats : S_Statistics
- * @param element : B_Byte 
+ * @param element : B_Byte
  * @return true if stats contains element
-*/
+ */
 bool S_contains(S_Statistics stats, B_Byte element);
 
 /**
@@ -43,29 +43,29 @@ bool S_contains(S_Statistics stats, B_Byte element);
  * @param stats : S_Statistics
  * @param element : B_Byte
  * @return the number of occurences of element in stats
-*/
-int S_getCount(S_Statistics stats, B_Byte element);
+ */
+int S_getCount(S_Statistics stats, int element);
 
 /**
  * @brief S_incCount
  * @param stats : S_Statistics
  * @param element : B_Byte
-*/
-void S_incCount(S_Statistics* stats, B_Byte element);
+ */
+void S_incCount(S_Statistics *stats, B_Byte element);
 
 /**
  * @brief S_length
  * @param stats : S_Statistics
  * @return the number of elements in stats
-*/
-int S_length(S_Statistics* stats);
+ */
+int S_length(S_Statistics *stats);
 
 /**
  * @brief S_getElement
  * @param stats : S_Statistics
  * @param pos : int
  * @return the element at position pos in stats
-*/
-B_Byte S_getElement(S_Statistics stats, int pos);
+ */
+int S_getElement(S_Statistics stats, int pos);
 
 #endif
