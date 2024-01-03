bf67c29604ed4b46d289b705a745b2946a7223ff
Mubelotix <mubelotix@gmail.com>
2024-01-03 15:08:29 +0100

    Add B_equals

--- programme/include/byte.h
+++ programme/include/byte.h
@@ -1,10 +1,11 @@
 #ifndef BYTE_H
 #define BYTE_H
 #include "binaryCode.h"
+#define bool int
 
 typedef struct{
     BC_Bit array[8];
-}B_Byte;
+} B_Byte;
 
 B_Byte B_byte(BC_Bit b1, BC_Bit b2, BC_Bit b3, BC_Bit b4, BC_Bit b5, BC_Bit b6, BC_Bit b7, BC_Bit b8);
 
@@ -13,5 +14,7 @@ BC_Bit B_getBit(B_Byte byte, unsigned int i);
 void B_setBit(B_Byte* byte, unsigned int i, BC_Bit bit);
 
 unsigned int B_byteToNatural(B_Byte byte);
+
+bool B_equals(B_Byte byte1, B_Byte byte2);
 
 #endif
