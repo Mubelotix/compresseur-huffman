a387f8adfc645172db80d1e7e809ad78d0021bbb
Florent TARIOLLE <florent.tariolle@insa-rouen.fr>
2024-01-03 15:54:12 +0100

    finished byte.c and modified byte.h for coherence

--- programme/include/byte.h
+++ programme/include/byte.h
@@ -1,7 +1,6 @@
 #ifndef BYTE_H
 #define BYTE_H
 #include "binaryCode.h"
-#define bool int
 
 typedef struct{
     BC_Bit array[8];
@@ -9,12 +8,12 @@ typedef struct{
 
 B_Byte B_byte(BC_Bit b1, BC_Bit b2, BC_Bit b3, BC_Bit b4, BC_Bit b5, BC_Bit b6, BC_Bit b7, BC_Bit b8);
 
-BC_Bit B_getBit(B_Byte byte, unsigned int i);
+BC_Bit B_getBit(B_Byte byte, int i);
 
-void B_setBit(B_Byte* byte, unsigned int i, BC_Bit bit);
+void B_setBit(B_Byte* byte, int i, BC_Bit bit);
 
 unsigned int B_byteToNatural(B_Byte byte);
 
-bool B_equals(B_Byte byte1, B_Byte byte2);
+int B_equals(B_Byte byte1, B_Byte byte2);
 
 #endif
