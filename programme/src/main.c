2e7482ea1f4d4c5e4b560b81ca161f6edd7c7928
angelo <angelo.bou_tanous@insa-rouen.fr>
2024-01-03 15:40:45 +0100

    développemtn du main.c (manque les include)

--- programme/src/main.c
+++ programme/src/main.c
@@ -1,3 +1,23 @@
-int main(){
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        printf("Error, lack of arguments");
+        return EXIT_FAILURE;
+    }
 
+    char option = argv[1][0];
+    const char* filename = argv[2];
+
+    switch (option) {
+        case 'c':
+            C_compressFile(filename);
+            break;
+        case 'd':
+            C_decompressFile(filename);
+            break;
+        default:
+            printf("Error, character c or d not found");
+            return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
