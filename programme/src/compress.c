2fc77c41582c5fb6aa5b59021566b0c1b110a94b
houssam <houssam.limy@insa-rouen.fr>
2024-01-03 22:36:23 +0100

    comp upd

--- programme/src/compress.c
+++ programme/src/compress.c
@@ -40,7 +40,10 @@ S_Statistics C_computeStatistics(FILE* file) {
     stats = S_statistics();
 
     B_Byte byte;
-    //while ((byte = fgetc(file)) != EOF) {       // si c un fichier d'octet faut trouver l'equivalent de fgetc
+    unsigned int byteNat;
+    //while ((byte = fgetc(file)) != EOF) 
+    while (fread(&byte, 1, 1, file) == 1){       // si c un fichier d'octet faut trouver l'equivalent de fgetc
+        byteNat = B_byteToNatural(byte);
     //    stats.element[byte]++;              // A modifier : fichier d'octet et freaquencies prend un [int] ?
     //}
     // Faut pouvoir recuperer les octets du fichier et les transformer en int
