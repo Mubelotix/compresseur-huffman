6777d23157f0afc95f629106f256dada5567ad13
Florent TARIOLLE <florent.tariolle@insa-rouen.fr>
2024-01-06 19:59:49 +0100

    produced isFileCompressed function

--- programme/src/decompress.c
+++ programme/src/decompress.c
@@ -2,3 +2,15 @@
 #include "huffmanTree.h"
 #include "binaryCode.h"
 #include <string.h>
+
+bool isFileCompressed(FILE *input)
+{
+    char identifier[4];
+    if (fread(identifier, sizeof(char), sizeof(identifier), input) != 4)
+        return false;
+
+    if (strncmp(identifier, "HUFF", sizeof(identifier)) != 0)
+        return false;
+
+    return true;
+}
