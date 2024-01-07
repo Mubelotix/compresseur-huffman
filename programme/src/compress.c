0b2239d6f0b43c60a83fd44444cdc1f99b453b95
Mubelotix <mubelotix@gmail.com>
2024-01-07 05:56:17 +0100

    Clean code

--- programme/src/compress.c
+++ programme/src/compress.c
@@ -9,45 +9,14 @@
 #include "binaryCode.h"
 #include "priorityQueue.h"
 
-    // 1. Changer type S_Statistics? : tableau d'occurence pour les 256 octets possible (FAIT ✅)
-//  typedef struct {
-//      int element[256];  
-//  } S_Statistics; //(FAIT ✅)
-
-
-    //(FAIT ✅)//2. Type HuffmanTreeNode : Faut faire en sorte de stocker l'octet aux feuilles de l'abres
-    // on rajoute un int octet qui prend -1 si c pas une feuille
-//struct HT_HuffmanTreeNode {
-//    int occurence;
-//    int octet;       //prend -1 si c un noeud et l'octet si feuille
-//    struct HT_HuffmanTreeNode* leftChild;
-//    struct HT_HuffmanTreeNode* rightChild;
-//};
-// Et du coup rajouter un getByte dans HT (FAIT ✅)
-
-// 3. Modifier HT_createLeaf de sorte qu'il initialise l'occurence de l'octet (FAIT ✅)
-
-// 4. Modifier HT_createNode de sorte qu'il calcule l'occurence (node.occurence = leftChild.occurence+rightChild.occurence)
-//                                      et met node.otet à -1(FAIT ✅)
-
-// 5.  Changer structure CT_CodingTable (FAIT ✅)
-// typedef struct {
-//    BC_BinaryCode table[256]; // Table de codage pour chaque octet
-// } CT_CodingTable;
-//Il sera don plus necessaire de passer par CT_coding pour faire l'association entre l'octet et son code binaire 
-
-
 S_Statistics C_computeStatistics(FILE* file) {
-    S_Statistics stats;
-
-    stats = S_statistics();
-
+    S_Statistics stats = S_statistics();
     B_Byte byte;
     unsigned int byteNat;
     char inputChar;
+    
     while (fread(&inputChar, 1, 1, file) == 1) {
-        byteNat = (unsigned int)inputChar;
-        byte = B_fromNatural(byteNat);
+        byte = B_fromNatural((unsigned int)inputChar);
         S_incCount(&stats, byte);
     }
 
@@ -62,8 +31,7 @@ void browseTree(HT_HuffmanTree noeud, BC_BinaryCode code, CT_CodingTable* coding
     // Leaves are saved in the coding table using the current code
     if (HT_isALeaf(noeud)) {
         B_Byte byte = HT_getByte(noeud);
-        unsigned int byte2 = B_byteToNatural(byte);
-        CT_add(codingTable,byte,code);
+        CT_add(codingTable, byte, code);
     }
 
     // Parcourir récursivement le sous-arbre gauche avec l'ajout de BC_ZERO au code binaire
@@ -85,13 +53,14 @@ CT_CodingTable C_buildCodingTable(HT_HuffmanTree tree) {
     return codingTable;
 }
 
-void writeData(FILE* soureFile, FILE* destFile, CT_CodingTable* table) {
+void writeData(FILE* sourceFile, FILE* destFile, CT_CodingTable* table) {
     B_Byte byte;
     unsigned int byteNat;
     char inputChar;
 
     BC_BinaryCode unsavedBits = BC_binaryCode();
-    while (fread(&inputChar, 1, 1, soureFile) == 1) {
+    fseek(sourceFile, 0, SEEK_SET);
+    while (fread(&inputChar, 1, 1, sourceFile) == 1) {
         byte = B_fromNatural(inputChar);
         BC_BinaryCode newBits = CT_getBinaryCode(*table, byte);
         BC_debug(newBits);
@@ -124,8 +93,8 @@ void C_compressFile(char* nameSourceFile) {
         exit(EXIT_FAILURE);
     }
 
-    FILE* tempFile = fopen("temp.huff", "wb"); // fichier destination "temp.huff" (a modifier)
-    if (tempFile == NULL) {
+    FILE* outputFile = fopen("temp.huff", "wb"); // fichier destination "temp.huff" (a modifier)
+    if (outputFile == NULL) {
         fclose(sourceFile);
         fprintf(stderr, "Erreur lors de la création du fichier temporaire.\n");
         exit(EXIT_FAILURE);
@@ -153,19 +122,17 @@ void C_compressFile(char* nameSourceFile) {
 
     // Write magic number
     char* id = "HUFFMAN";
-    fwrite(id, sizeof(char), strlen(id), tempFile);
+    fwrite(id, sizeof(char), strlen(id), outputFile);
 
     // Save statistics
-    S_save(stats, tempFile);
-
-    fseek(sourceFile, 0, SEEK_SET);
+    S_save(stats, outputFile);
 
     printf("\nWriting data...\n");
-    writeData(sourceFile, tempFile, &table); //ecrire donnees compress
+    writeData(sourceFile, outputFile, &table); //ecrire donnees compress
 
     // Fermer les fichiers et libérer la mémoire
     fclose(sourceFile);
-    fclose(tempFile);
+    fclose(outputFile);
     
     HT_destroy(huffmanTree);
 }
