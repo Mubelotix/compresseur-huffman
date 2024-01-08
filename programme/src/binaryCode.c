615756b4b84673a9ddb177ec2bc436afcd7772a3
qberal <quentin.beral@insa-rouen.fr>
2024-01-08 01:46:37 +0100

    fix warning

--- programme/src/binaryCode.c
+++ programme/src/binaryCode.c
@@ -10,12 +10,11 @@ BC_BinaryCode BC_binaryCode() {
     return bc;
 }
 
-void BC_addBit(BC_BinaryCode* pbc, BC_Bit b) {
-    if (pbc->length < BC_MAX){
+void BC_addBit(BC_BinaryCode *pbc, BC_Bit b) {
+    if (pbc->length < BC_MAX) {
         pbc->bits[pbc->length] = b;
         pbc->length++;
-    }
-    else{
+    } else {
         errno = EOVERFLOW;
     }
 }
@@ -25,48 +24,48 @@ unsigned int BC_getLength(BC_BinaryCode bc) {
 }
 
 BC_Bit BC_getBit(BC_BinaryCode bc, unsigned int pos) {
-    if(pos < bc.length) {
+    if (pos < bc.length) {
         return bc.bits[pos];
-    }
-    else {
+    } else {
         errno = EDOM;
-        return;
+        return -1;
     }
 }
 
-void BC_concatenate(BC_BinaryCode* pbc1, BC_BinaryCode* pbc2) {
+void BC_concatenate(BC_BinaryCode *pbc1, BC_BinaryCode *pbc2) {
     unsigned int i;
-    for (i=0; i < BC_getLength(*pbc2); i++) {
-        BC_addBit(pbc1, BC_getBit(*pbc2,i));
+    for (i = 0; i < BC_getLength(*pbc2); i++) {
+        BC_addBit(pbc1, BC_getBit(*pbc2, i));
     }
 }
 
-B_Byte BC_removeFirstByte(BC_BinaryCode* pbc) {
+B_Byte BC_removeFirstByte(BC_BinaryCode *pbc) {
     if (BC_getLength(*pbc) < 8) {
         printf("Error : BinaryCode length is less than 8. \n");
         return B_byte(0, 0, 0, 0, 0, 0, 0, 0);
     }
-    
-    B_Byte res = B_byte(pbc->bits[0], pbc->bits[1], pbc->bits[2], pbc->bits[3], pbc->bits[4], pbc->bits[5], pbc->bits[6], pbc->bits[7]);
 
-    for (unsigned int i=0; i < BC_getLength(*pbc)-8; i++) {
-        pbc->bits[i] = pbc->bits[i+8];
+    B_Byte res = B_byte(pbc->bits[0], pbc->bits[1], pbc->bits[2], pbc->bits[3], pbc->bits[4], pbc->bits[5],
+                        pbc->bits[6], pbc->bits[7]);
+
+    for (unsigned int i = 0; i < BC_getLength(*pbc) - 8; i++) {
+        pbc->bits[i] = pbc->bits[i + 8];
     }
     pbc->length -= 8;
 
     return res;
 }
 
-void BC_appendByte(BC_BinaryCode* pbc, B_Byte byte) {
+void BC_appendByte(BC_BinaryCode *pbc, B_Byte byte) {
     unsigned char byteNat = B_byteToNatural(byte);
-    for (unsigned int i=0; i < 8; i++) {
-        BC_addBit(pbc, (byteNat >> (7-i)) & 1);
+    for (unsigned int i = 0; i < 8; i++) {
+        BC_addBit(pbc, (byteNat >> (7 - i)) & 1);
     }
 }
 
 BC_BinaryCode BC_prefix(BC_BinaryCode bc, unsigned int length) {
     BC_BinaryCode res = BC_binaryCode();
-    for (unsigned int i=0; i < length; i++) {
+    for (unsigned int i = 0; i < length; i++) {
         BC_addBit(&res, BC_getBit(bc, i));
     }
     return res;
@@ -74,7 +73,7 @@ BC_BinaryCode BC_prefix(BC_BinaryCode bc, unsigned int length) {
 
 BC_BinaryCode BC_suffix(BC_BinaryCode bc, unsigned int start) {
     BC_BinaryCode res = BC_binaryCode();
-    for (unsigned int i=start; i < BC_getLength(bc); i++) {
+    for (unsigned int i = start; i < BC_getLength(bc); i++) {
         BC_addBit(&res, BC_getBit(bc, i));
     }
     return res;
@@ -84,8 +83,8 @@ bool BC_equals(BC_BinaryCode bc1, BC_BinaryCode bc2) {
     if (bc1.length != bc2.length) {
         return 0;
     } else {
-        for (unsigned int i=0; i < bc1.length; i++){
-            if (bc1.bits[i] != bc2.bits[i]){
+        for (unsigned int i = 0; i < bc1.length; i++) {
+            if (bc1.bits[i] != bc2.bits[i]) {
                 return 0;
             }
         }
@@ -94,7 +93,7 @@ bool BC_equals(BC_BinaryCode bc1, BC_BinaryCode bc2) {
 }
 
 void BC_debug(BC_BinaryCode bc) {
-    for (unsigned int i=0; i < bc.length; i++){
+    for (unsigned int i = 0; i < bc.length; i++) {
         printf("%d", bc.bits[i]);
     }
 }
